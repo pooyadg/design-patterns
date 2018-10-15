b06795b8970e64180408ee09cd0709f14ad45237
Pooya D. GOhardani <p.deldar@tax.gov.ir>
2018-10-15 19:44:22 +0330

    Proxy pattern.

---
+++ src/com/gohardani/samples/patterns/structural/proxy/CommandExecutor.java
@@ -0,0 +1,6 @@
+package com.gohardani.samples.patterns.structural.proxy;
+
+public interface CommandExecutor {
+
+    public void runCommand(String cmd) throws Exception;
+}
---
+++ src/com/gohardani/samples/patterns/structural/proxy/ProxyPatternTest.java
@@ -0,0 +1,15 @@
+package com.gohardani.samples.patterns.structural.proxy;
+
+public class ProxyPatternTest {
+
+    public static void main(String[] args) {
+        CommandExecutor executor = new CommandExecutorProxy("username", "wrong_pwd");
+        try {
+            executor.runCommand("ls -ltr");
+            executor.runCommand(" rm -rf abc.pdf");
+        } catch (Exception e) {
+            System.out.println("Exception Message::" + e.getMessage());
+        }
+
+    }
+}
